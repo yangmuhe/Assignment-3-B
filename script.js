88eed83e26173b751528893a37775538bdfb5a81
siqister <siqi.zhu@gmail.com>
2015-10-16 09:25:27 -0400

    Ready for distribution

--- script.js
+++ script.js
@@ -17,14 +17,8 @@ var plot = d3.select('#plot')
 //Consult documentation here https://github.com/mbostock/d3/wiki/SVG-Axes
 var scaleX,scaleY;
 
-var axisX = d3.svg.axis()
-    .orient('bottom')
-    .tickSize(-height)
-    .tickValues([10000,50000,100000]);
-var axisY = d3.svg.axis()
-    .orient('left')
-    .tickSize(-width)
-    .tickValues([0,25,50,75,100]);
+var axisX;
+var axisY;
 
 
 //Start importing data
@@ -33,80 +27,35 @@ d3.csv('/data/world_bank_2012.csv', parse, dataLoaded);
 function parse(d){
 
     //Eliminate records for which gdp per capita isn't available
-    if(d['GDP per capita, PPP (constant 2011 international $)']=='..'){
-        return;
-    }
 
+    //Check "primary completion" and "urban population" columns
+    //if figure is unavailable and denoted as "..", replace it with undefined
+    //otherwise, parse the figure into numbers
     return {
-        cName: d['Country Name'],
-        cCode: d['Country Code'],
-        gdpPerCap: +d['GDP per capita, PPP (constant 2011 international $)'],
-        primaryCompletion: d['Primary completion rate, total (% of relevant age group)']!='..'?+d['Primary completion rate, total (% of relevant age group)']:undefined,
-        urbanPop: d['Urban population (% of total)']!='..'?+d['Urban population (% of total)']:undefined
-    }
+
+    };
+
+
+
 }
 
 function dataLoaded(error, rows){
     //with data loaded, we can now mine the data
-    var gdpPerCapMin = d3.min(rows, function(d){return d.gdpPerCap}),
-        gdpPerCapMax = d3.max(rows, function(d){return d.gdpPerCap});
 
-    //with mined information, we can now set up the scales
-    scaleX = d3.scale.log().domain([gdpPerCapMin,gdpPerCapMax]).range([0,width]),
-    scaleY = d3.scale.linear().domain([0,150]).range([height,0]);
 
-    //Once we know the scales, we can represent the scales with axes
-    axisX.scale(scaleX);
-    axisY.scale(scaleY);
 
-    plot.append('g')
-        .attr('class','axis axis-x')
-        .attr('transform','translate(0,'+height+')')
-        .call(axisX);
+    //with mined information, set up domain and range for x and y scales
+    //Log scale for x, linear scale for y
+    //scaleX = d3.scale.log()...
 
-    plot.append('g')
-        .attr('class','axis axis-y')
-        .call(axisY);
 
-    //draw
-    var nodes = plot.selectAll('.country')
-        .data(rows,function(d){return d.cCode})
-        .enter()
-        .append('g')
-        .attr('class','country')
-        .attr('transform',function(d){
-            return 'translate('+ scaleX(d.gdpPerCap) + ',0)';
-        })
-        .on('click',function(d){
-            console.log(d);
-        })
 
-    nodes
-        .filter(function(d){
-            return d.primaryCompletion!=undefined;
-        })
-        .append('line')
-        .attr('class','prim-comp')
-        .attr('y1',function(d){
-            return scaleY(d.primaryCompletion)
-        })
-        .attr('y2',height)
-        .style('stroke-width','2px')
-        .style('stroke','#03afeb')
-        .style('stroke-opacity',.7)
+    //Draw axisX and axisY
 
-    nodes
-        .append('line')
-        .attr('class','urban-pop')
-        .attr('y1',function(d){
-            if(!d.urbanPop){return height;}
-            return scaleY(d.urbanPop)
-        })
-        .attr('y2',height)
-        .style('stroke-width','1px')
-        .style('stroke','red')
-        .style('stroke-opacity',.7);
 
+
+    //draw <line> elements to represent countries
+    //each country should have two <line> elements, nested under a common <g> element
 
 }
 
